4d52ee481291a8e45979724d0c6db18ebd9fb8b6
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-07-09 14:19:09 +0200

    validation module completed

--- src/main/java/com/lyes/auth/validators/userValidator.java
+++ src/main/java/com/lyes/auth/validators/userValidator.java
@@ -1,12 +1,18 @@
 package com.lyes.auth.validators;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.lyes.auth.model.User;
+import com.lyes.auth.services.UserService;
 
 public class userValidator implements Validator {
-
+	
+	@Autowired 
+	private UserService userService;
+		
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return User.class.equals(clazz);
@@ -14,7 +20,25 @@ public class userValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
+		User user = (User)target;
 		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+		if(user.getUsername().length() < 6 || user.getUsername().length() > 32) {
+			errors.rejectValue("username", "Size.userForm.username");
+		}
+		
+		if(userService.findUsername(user.getUsername()) != null) {
+			errors.rejectValue("username", "Duplicate.userForm.username");
+		}
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+		if(user.getPassword().length() < 6 || user.getPassword().length() > 32) {
+			errors.rejectValue("password", "Size.userForm.password");
+		}
+		
+		if(!user.getPasswordConfirm().equals(user.getPassword())) {
+			errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+		}
 	}
 
 }
